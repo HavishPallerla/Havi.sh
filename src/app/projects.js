baed431f785145c756832e6455663c36cf6838ea
havishpallerla <hey@havi.sh>
2025-03-26 00:24:56 -0400

    edited projects, blog

---
+++ src/app/projects.js
@@ -0,0 +1,9 @@
+export default function Projects() {
+    return (
+      <div className="flex items-center justify-center min-h-screen">
+        <p className="text-xl font-medium">
+          Projects page is in development!
+        </p>
+      </div>
+    );
+  }
