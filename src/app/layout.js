0edd0d0e62779e59973081994bd191f7fca91373
havishpallerla <hey@havi.sh>
2026-01-02 02:47:47 -0500

    logo

--- src/app/layout.js
+++ src/app/layout.js
@@ -10,7 +10,7 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <head>
-        <link rel="icon" href="/logo-icon.png" type="image/png" />
+        <link rel="icon" href="/logo.png" type="image/png" />
       </head>
       <body className="antialiased transition-colors duration-300">
         {children}
