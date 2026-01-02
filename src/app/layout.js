e5611f0a750b2cc404aee7a812cd7ed099c11b6a
havishpallerla <hey@havi.sh>
2026-01-02 03:23:36 -0500

    head

--- src/app/layout.js
+++ src/app/layout.js
@@ -11,7 +11,6 @@ export default function RootLayout({ children }) {
     <html lang="en">
       <head>
         <meta name="viewport" content="width=device-width, initial-scale=1" />
-        <link rel="icon" href="/logo-icon.png" type="image/png" />
       </head>
       <body className="antialiased transition-colors duration-300">
         {children}
